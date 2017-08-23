5fccf1045910bd5cd1e78db8c0136c8153d21393
rextw-priv <rexx0520test@gmail.com>
2017-08-23 10:36:28 +0800

    中括號換成小括號

--- bot.py
+++ bot.py
@@ -123,7 +123,7 @@ def getMusicId(musicInfo):
     if musicInfo.isnumeric():
         return musicInfo
     elif "//music.163.com/song/" in musicInfo:
-        return musicInfo.split['//music.163.com/song/'][1].split['/'][0]
+        return musicInfo.split('//music.163.com/song/')[1].split('/')[0]
     elif "song?id=" in musicInfo:
         return ''.join(i for i in musicInfo.split('id=')[1] if i.isdigit())
     else:
